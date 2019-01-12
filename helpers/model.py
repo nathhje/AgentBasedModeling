42adeef832c9f5cbb3792798a00a396e0a9b530f
jasperdenduijf <31652548+jasperdenduijf@users.noreply.github.com>
2019-01-12 16:31:12 +0100

    Model versie 1

--- helpers/model.py
+++ helpers/model.py
@@ -5,6 +5,42 @@ Created on Sat Jan 12 09:44:23 2019
 @author: Gebruiker
 """
 
-def model():
+import Agent
+
+class Model():
     
-    print("This will contain the model")
+	def __init__(self):
+	
+		self.time = 0
+		self.end_time = 100
+		self.agent_list = []
+		self.winner_history = []
+		
+	def run_simulation(self):
+		
+		while(self.time < self.end_time):
+			this_round = 0
+			for agent in agent_list:
+				if agent.choose():
+					this_round += 1
+				
+			if (this_round > len(self.agent_list) / 2):
+				self.winner_history.append(True)
+			else:
+				self.winner_history.append(False)
+		
+			for agent in agent_list:
+				agent.outcome(self.winner_history[-1])
+				
+			self.time += 1
+	
+	def make_agents(self, number_of_agents):
+		
+		for i in range(number_of_agents):
+			self.agent_list.append(Agent())
+	
+modelA = Model()
+modelA.make_agents(10)
+modelA.run_simulation()
+
+print(modelA.winner_history)
