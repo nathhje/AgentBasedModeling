b53ffec99c13446e99129d1ac3a9775e4cc4e02d
martin <martinfrassek@gmx.de>
2019-01-16 12:40:31 +0100

    Strategy added that determines buying/selling price by a weighted average of linear predictions of the last n, n-1, n-2... 2 and the last stock values.

--- classes/strategiessellers.py
+++ classes/strategiessellers.py
@@ -10,63 +10,71 @@ import math
 
 class StrategiesSellers():
 
-    def __init__(self):
-        self.threshold_low_price = 8
-        self.threshold_high_price = 12
-        self.n_time = 5
+	def __init__(self):
+		self.threshold_low_price = 8
+		self.threshold_high_price = 12
+		self.n_time = 7
+		self.market_memory = 3
+		self.weights_dict = {0:0, 1:3, 2:1, 3:1}
 
-    def sell_optimistic(self, history_self, history_all):
-        """Agent tries to sell for 1 more than the market value last turn."""
-        self.sell_price = history_all[-1] + 1
-        return self.sell_price
+	def sell_optimistic(self, history_self, history_all):
+		"""Agent tries to sell for 1 more than the market value last turn."""
+		self.sell_price = history_all[-1] + 1
+		return self.sell_price
 
-    def sell_pesimistic(self, history_self, history_all):
-        """Agent tries to sell for 1 less than the market value last turn."""
-        self.sell_price = history_all[-1] - 1
-        return self.sell_price
+	def sell_pesimistic(self, history_self, history_all):
+		"""Agent tries to sell for 1 less than the market value last turn."""
+		self.sell_price = history_all[-1] - 1
+		return self.sell_price
 
-    def sell_predict_last_2(self, history_self, history_all):
-        """Agent predicts the new selling price based on the pregression 
-        of the last two steps. """
-        if len(history_all) >= 2:
-            self.sell_price = history_all[-1] + (history_all[-1]-history_all[-2])
-        else:
-            self.sell_price = history_all[-1]
-        return self.sell_price
+	def sell_predict_last_2(self, history_self, history_all):
+		"""Agent predicts the new selling price based on the pregression 
+		of the last two steps. """
+		if len(history_all) >= 2:
+			self.sell_price = history_all[-1] + (history_all[-1]-history_all[-2])
+		else:
+			self.sell_price = history_all[-1]
+		return self.sell_price
 
-    def sell_predict_last_n(self, history_self, history_all):
-        """Agent predicts the new selling price based on the pregression 
-        of the last two steps. """
-        print(self.n_time)
-        if len(history_all) >= 2:
-            self.sell_price = history_all[-1] + (history_all[-1]-history_all[-2])
-        else:
-            self.sell_price = history_all[-1]
-        return self.sell_price
-
-
-
+	def sell_predict_weighted_last_n(self, history_self, history_all):
+		"""Agent predicts the new selling price based on the progression 
+		of the last n, n-1, n-2..., 2 steps. n is determined by the variable 
+		self.market_memory and the length of history_all. The selling price 
+		determined is a weighted average of the predicitons for the different lines.
+		The weights are determined are stored in weights_dict. 
+		"""
+		if self.market_memory >= len(history_all):
+			max_length_of_line = len(history_all)
+		else:
+			max_length_of_line = self.market_memory
+		sum_sell_price = 0
+		sum_weights = 0
+		for i in range(max_length_of_line):
+			sum_sell_price += self.weights_dict[i] * (history_all[-1] + (history_all[-1]-history_all[-(i+1)]))
+			sum_weights += self.weights_dict[i]
+		self.sell_price = sum_sell_price/sum_weights
+		return self.sell_price
 
 """Sellers strategies"""
 """ #Agent sell if current price is above the threshold
-    def sell_strat_above(self, history_self, history_all):
-        if history_all[-1] > self.threshold_high_price:
-            self.choice = -1
-        else:
-            self.choice = self.do_nothing_strat(history_self,history_all)
-        return self.choice
+	def sell_strat_above(self, history_self, history_all):
+		if history_all[-1] > self.threshold_high_price:
+			self.choice = -1
+		else:
+			self.choice = self.do_nothing_strat(history_self,history_all)
+		return self.choice
 
-    #Agent sell if after n times dropping the price, the price rices
-    def sell_strat_after(self,history_self,history_all):
-        self.choice = -1
-        highest_price = history_all[-self.n_time]
-        for price in history_all[-self.n_time+1:-1]:
-            if(highest_price < price):
-                highest_price = price
-            else:
-                self.choice = 0
-        return self.choice
+	#Agent sell if after n times dropping the price, the price rices
+	def sell_strat_after(self,history_self,history_all):
+		self.choice = -1
+		highest_price = history_all[-self.n_time]
+		for price in history_all[-self.n_time+1:-1]:
+			if(highest_price < price):
+				highest_price = price
+			else:
+				self.choice = 0
+		return self.choice
 
-    def sell_strat_hist_time(self,history_self,history_all):
-        return self.choice
+	def sell_strat_hist_time(self,history_self,history_all):
+		return self.choice
 """
--- test_Martin.py
+++ test_Martin.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 def main():
     
 	history = []
-	history_all = [8, 10]
+	history_all = [8, 10, 11, 10]
 	strat_sell = StrategiesSellers()
-	print(strat_sell.sell_predict_last_n(history, history_all))
+	print(strat_sell.sell_predict_weighted_last_n(history, history_all))
 	
     
 if __name__ == "__main__":
