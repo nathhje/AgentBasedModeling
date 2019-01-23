291a7d511bf108bad11c13439b83c4bcc653b834
nathhje <n.van.sburg@hotmail.nl>
2019-01-23 13:54:14 +0100

    multiple strategies added

--- classes/secretAgent.py
+++ classes/secretAgent.py
@@ -12,94 +12,105 @@ from classes.strategiesbuyers import StrategiesBuyers
 from classes.strategiessellers import StrategiesSellers
 
 class Agent():
+    
+    def __init__(self, seller):
+        
+        self.seller = seller
+        self.buy_prices = []
+        self.sell_prices = []
 	
-	def __init__(self, seller):
+        self.buy_strategies = StrategiesBuyers()
+        self.sell_strategies = StrategiesSellers()
 	
-		self.score = 0
-		self.score_list = []
-	
-		self.seller = seller
-		self.buy_prices = []
-		self.sell_prices = []
-	
-		self.buy_strategies = StrategiesBuyers()
-		self.sell_strategies = StrategiesSellers()
-	
-		self.memory = math.floor(random.random()*80 + 0.5)
-		self.weights = self.setWeights(self.memory)
+        self.number_of_strategies = 5
+        self.strategies = []
+        self.create_strategies()
+        #self.memory = math.floor(random.random()*80 + 0.5)
+        #self.weights = self.setWeights(self.memory)
 
-		self.match_prices = []
-		self.profit = 0
-		self.penalty = 0
+        self.match_prices = []
+        self.profit = 0
+        self.penalty = 0
+    
+    def create_strategies(self):
+        
+        for i in range(self.number_of_strategies):
+            self.strategies.append([])
+            memory = math.floor(random.random()*80 + 0.5)
+            self.strategies[i].append(memory)
+            self.strategies[i].append(self.setWeights(memory))
+            
+    def choose_strategy(self):
+        
+        index = math.floor(random.random()*len(self.strategies))
+        
+        return self.strategies[index][1]
+        
 		
-	def update(self, winner, price):
-		self.score_list.append(winner)
-		if winner == True:
-			self.score += 1
-		else:
-			self.score += -1
-		
-	def random_choose(self, stock_price_history):
+    def random_choose(self, stock_price_history):
 	
-		self.match_prices.append(0)
-		if self.seller == True:
+        self.match_prices.append(0)
+        if self.seller == True:
 		
-			sell_price = stock_price_history[-1] + random.random() - 0.4
-			self.sell_prices.append(sell_price)
-			return sell_price
+            sell_price = stock_price_history[-1] + random.random() - 0.4
+            self.sell_prices.append(sell_price)
+            return sell_price
 			
-		else:
+        else:
 		
-			buy_price = stock_price_history[-1] + random.random() - 0.6
-			self.buy_prices.append(buy_price)
-			return buy_price
+            buy_price = stock_price_history[-1] + random.random() - 0.6
+            self.buy_prices.append(buy_price)
+            return buy_price
 	
-	def setWeights(self, visionRange):
-		weightList = []
-		for i in range(visionRange - 1):
-			weightList.append(random.random())
-		for i in range(visionRange - 1):
-			weightList[i] = weightList[i]/sum(weightList)*len(weightList)
+    def setWeights(self, visionRange):
+        weightList = []
+        for i in range(visionRange - 1):
+            weightList.append(random.random())
+        for i in range(visionRange - 1):
+            weightList[i] = weightList[i]/sum(weightList)*len(weightList)
 			
-		return weightList
+        return weightList
 	
-	def nextPoint(self, weightList, setPoints):	
-		visionRange = len(weightList)
+    def nextPoint(self, weightList, setPoints):	
+        visionRange = len(weightList)
 		
-		if visionRange <= 1:
-			return setPoints[-1]
+        if visionRange <= 1:
+            return setPoints[-1]
 		
-		newpoint = setPoints[-visionRange]
-		for i in reversed(range(visionRange)):
-			b = (i + 1) * -1
-			newpoint += weightList[i]*(setPoints[b] - setPoints[b-1])
+        newpoint = setPoints[-visionRange]
+        for i in reversed(range(visionRange)):
+            b = (i + 1) * -1
+            newpoint += weightList[i]*(setPoints[b] - setPoints[b-1])
 			
-		return newpoint
+        return newpoint
 	
-	def choose(self, stock_price_history):
+    def choose(self, stock_price_history):
+        
+        weights = self.choose_strategy()
 	
 		#add for every turn a new 0
-		self.match_prices.append(0)
+        self.match_prices.append(0)
 		
-		picksomething = self.nextPoint(self.weights, stock_price_history)
-		self.sell_prices.append(picksomething)
-		self.buy_prices.append(picksomething)
-		return picksomething
+        picksomething = self.nextPoint(weights, stock_price_history)
+        self.sell_prices.append(picksomething)
+        self.buy_prices.append(picksomething)
+        return picksomething
 		
-	def matched(self, price):
-		self.match_prices[-1] = price
+    def matched(self, price):
+        self.match_prices[-1] = price
 		
-	def calcProfit(self, marketprice):
-		if self.match_prices[-1] == 0:
-			self.profit += self.penalty
-			return
+    def calcProfit(self, marketprice):
+        if self.match_prices[-1] == 0:
+            self.profit += self.penalty
+            return
 		
-		if len(self.match_prices) < 1:
-			return
+        # Can this be removed?
+        if len(self.match_prices) < 1:
+            return
 		
-		if self.seller:
-			self.profit += self.match_prices[-1] - marketprice
-		else:
-			self.profit += marketprice - self.match_prices[-1]
+        if self.seller:
+            self.profit += self.match_prices[-1] - marketprice
+        else:
+            self.profit += marketprice - self.match_prices[-1]
 			
     
