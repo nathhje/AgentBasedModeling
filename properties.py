20462cdd8e31d7ef2c8488f2b95508508f9cca57
Nicole <Nicole-de-groot@users.noreply.github.com>
2019-02-06 12:17:22 +0100

    Properties
    Needs to be integrated

---
+++ properties.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 09:50:10 2019
+
+@author: Nicole 
+"""
+
+max_runtime = 1000
+number_of_buyers = 50
+number_of_sellers = 50
+warming_up_time = 100
+number_of_wu_agents = 40
+initial_stockprice = 10
+ratio_of_random_agents = 0.5
