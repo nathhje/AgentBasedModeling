4a185e63b91dbb53e2aed763d199d73cf421dc2d
Nicole <Nicole-de-groot@users.noreply.github.com>
2019-02-07 12:57:33 +0100

    Update testfiles

--- test_code.py
+++ test_code.py
@@ -1,9 +1,9 @@
 """This will be a test file to test the code"""
 
-from classes.agent3 import Agent
+from classes.agentv7 import Agent
 
-agent1 = Agent(True)
-agent2 = Agent(False)
+agent1 = Agent(True,1,1,False)
+agent2 = Agent(False,1,1,False)
 
 def test_seller():
 	assert agent1.seller == True
--- test_nicole.py
+++
@@ -1,9 +0,0 @@
-from classes.agent3 import Agent
-
-def main():
-	agent = Agent(True)
-	agent.choose([10])
-	print(agent.sell_prices)
-
-if __name__ == "__main__":
-    main()
